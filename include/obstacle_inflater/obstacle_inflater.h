ef96208c86c491b17a2b00283f8d9d1fe2d600a6
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-05-04 10:05:04 +0900

    Create obstacle inflater

---
+++ include/obstacle_inflater/obstacle_inflater.h
@@ -0,0 +1,163 @@
+/**
+ * @file obstacle_inflater.h
+ * @author Toshiki Nakamura
+ * @brief C++ implementation of obstacle inflater
+ * @date 2024-05-04
+ * @copyright Copyright (c) 2024
+ */
+
+#ifndef OBSTACLE_INFLATER_OBSTACLE_INFLATER_H
+#define OBSTACLE_INFLATER_OBSTACLE_INFLATER_H
+
+#include <nav_msgs/OccupancyGrid.h>
+#include <ros/ros.h>
+#include <vector>
+
+/**
+ * @class ObstacleInflater
+ * @brief Class for inflating obstacles
+ */
+class ObstacleInflater
+{
+public:
+  /**
+   * @brief Construct a new Obstacle Inflater object
+   *
+   */
+  ObstacleInflater();
+
+  /**
+   * @brief Process the inflation
+   *
+   */
+  void process();
+
+private:
+  /**
+   * @brief Callback function for map
+   *
+   * @param msg map message
+   */
+  void map_callback(const nav_msgs::OccupancyGrid::ConstPtr &msg);
+
+  /**
+   * @brief Inflate obstacles
+   *
+   * @param map map message
+   */
+  void inflate_obstacle(const nav_msgs::OccupancyGrid &map);
+
+  /**
+   * @brief Change surrounding grid color
+   *
+   * @param center_grid_index center grid index
+   * @param target_margin target margin
+   * @param map map message
+   */
+  void
+  change_surrounding_grid_color(const int center_grid_index, const float target_margin, nav_msgs::OccupancyGrid &map);
+
+  /**
+   * @brief Check if the grid is in the margin
+   *
+   * @param index grid index
+   * @param center_grid_index center grid index
+   * @param target_margin target margin
+   * @param map map message
+   * @return true if the grid is in the margin
+   * @return false if the grid is not in the margin
+   */
+  bool in_margin(
+      const int index, const int center_grid_index, const float target_margin, const nav_msgs::OccupancyGrid &map);
+
+  /**
+   * @brief Calculate distance between two grids
+   *
+   * @param index1 grid index 1
+   * @param index2 grid index 2
+   * @param map map
+   * @return float distance between two grids
+   */
+  float calc_dist_between_grid(const int index1, const int index2, const nav_msgs::OccupancyGrid &map);
+
+  /**
+   * @brief Get the list of grid index in the rectangle
+   *
+   * @param center_grid_index center grid index
+   * @param target_margin target margin
+   * @param map map
+   * @return std::vector<int> list of grid index in the rectangle
+   */
+  std::vector<int>
+  get_rect_grid_index_list(const int center_grid_index, const float target_margin, const nav_msgs::OccupancyGrid &map);
+
+  /**
+   * @brief Get the upper right grid index
+   *
+   * @param center_grid_index center grid index
+   * @param target_margin target margin
+   * @param map map
+   * @return int upper right grid index
+   */
+  int get_upper_right_grid_index(
+      const int center_grid_index, const float target_margin, const nav_msgs::OccupancyGrid &map);
+
+  /**
+   * @brief Get the lower left grid index object
+   *
+   * @param center_grid_index center grid index
+   * @param target_margin target margin
+   * @param map map
+   * @return int lower left grid index
+   */
+  int get_lower_left_grid_index(
+      const int center_grid_index, const float target_margin, const nav_msgs::OccupancyGrid &map);
+
+  /**
+   * @brief Calculate grid index x
+   *
+   * @param index grid index
+   * @param map map
+   * @return int grid index x
+   */
+  int calc_grid_index_x(const int index, const nav_msgs::OccupancyGrid &map);
+
+  /**
+   * @brief Calculate grid index y
+   *
+   * @param index grid index
+   * @param map map
+   * @return int grid index y
+   */
+  int calc_grid_index_y(const int index, const nav_msgs::OccupancyGrid &map);
+
+  /**
+   * @brief Calculate max grid index in the same line
+   *
+   * @param index grid index
+   * @param map map
+   * @return int max grid index in the same line
+   */
+  int calc_max_grid_index_in_same_line(const int index, const nav_msgs::OccupancyGrid &map);
+
+  /**
+   * @brief Calculate min grid index in the same line
+   *
+   * @param index grid index
+   * @param map map
+   * @return int min grid index in the same line
+   */
+  int calc_min_grid_index_in_same_line(const int index, const nav_msgs::OccupancyGrid &map);
+
+  int hz_;
+  float inflation_radius_;
+
+  ros::NodeHandle nh_;
+  ros::NodeHandle private_nh_;
+  ros::Publisher inflated_map_pub_;
+  ros::Subscriber raw_map_sub_;
+
+  nav_msgs::OccupancyGrid inflated_map_;
+};
+
+#endif  // OBSTACLE_INFLATER_OBSTACLE_INFLATER_H
---
+++ src/obstacle_inflater.cpp
@@ -0,0 +1,179 @@
+/**
+ * @file obstacle_inflater.cpp
+ * @author Toshiki Nakamura
+ * @brief C++ implementation of obstacle inflater
+ * @date 2024-05-04
+ * @copyright Copyright (c) 2024
+ */
+
+#include <algorithm>
+#include <vector>
+
+#include "obstacle_inflater/obstacle_inflater.h"
+
+ObstacleInflater::ObstacleInflater() : private_nh_("~")
+{
+  private_nh_.param<int>("hz", hz_, 1);
+  private_nh_.param<float>("inflation_radius", inflation_radius_, 0.15);
+
+  inflated_map_pub_ = nh_.advertise<nav_msgs::OccupancyGrid>("/map/inflated_map", 1);
+  raw_map_sub_ = nh_.subscribe("/map", 1, &ObstacleInflater::map_callback, this);
+}
+
+void ObstacleInflater::process()
+{
+  ros::Rate loop_rate(hz_);
+  while (ros::ok())
+  {
+    ros::spinOnce();
+    inflated_map_pub_.publish(inflated_map_);
+    loop_rate.sleep();
+  }
+}
+
+void ObstacleInflater::map_callback(const nav_msgs::OccupancyGrid::ConstPtr &msg) { inflate_obstacle(*msg); }
+
+void ObstacleInflater::inflate_obstacle(const nav_msgs::OccupancyGrid &map)
+{
+  inflated_map_ = map;
+  for (int i = 0; i < map.data.size(); i++)
+  {
+    if (map.data[i] == 100)
+      change_surrounding_grid_color(i, inflation_radius_, inflated_map_);
+  }
+}
+
+void ObstacleInflater::change_surrounding_grid_color(
+    const int center_grid_index, const float target_margin, nav_msgs::OccupancyGrid &map)
+{
+  const std::vector<int> rect_grid_index_list = get_rect_grid_index_list(center_grid_index, target_margin, map);
+  for (const auto &index : rect_grid_index_list)
+  {
+    if (in_margin(index, center_grid_index, target_margin, map))
+    {
+      if (index < map.data.size())
+        map.data[index] = 100;
+      else
+        ROS_WARN("index out of range");
+    }
+  }
+}
+
+bool ObstacleInflater::in_margin(
+    const int index, const int center_grid_index, const float target_margin, const nav_msgs::OccupancyGrid &map)
+{
+  const float dist = calc_dist_between_grid(center_grid_index, index, map);
+  return dist <= target_margin;
+}
+
+float ObstacleInflater::calc_dist_between_grid(const int index1, const int index2, const nav_msgs::OccupancyGrid &map)
+{
+  const int index1_x = calc_grid_index_x(index1, map);
+  const int index1_y = calc_grid_index_y(index1, map);
+  const int index2_x = calc_grid_index_x(index2, map);
+  const int index2_y = calc_grid_index_y(index2, map);
+
+  return hypot(index1_x - index2_x, index1_y - index2_y) * map.info.resolution;
+}
+
+std::vector<int> ObstacleInflater::get_rect_grid_index_list(
+    const int center_grid_index, const float target_margin, const nav_msgs::OccupancyGrid &map)
+{
+  const int upper_right_grid_index = get_upper_right_grid_index(center_grid_index, target_margin, map);
+  const int lower_left_grid_index = get_lower_left_grid_index(center_grid_index, target_margin, map);
+
+  const int width = calc_grid_index_x(upper_right_grid_index, map) - calc_grid_index_x(lower_left_grid_index, map);
+  const int height = calc_grid_index_y(upper_right_grid_index, map) - calc_grid_index_y(lower_left_grid_index, map);
+
+  if (width < 0 || height < 0)
+  {
+    ROS_WARN("width or height is negative");
+    return std::vector<int>();
+  }
+
+  std::vector<int> rect_grid_index_list;
+  for (int i = 0; i < height; i++)
+  {
+    int index = lower_left_grid_index + map.info.width * i;
+    for (int j = 0; j < width; j++)
+    {
+      rect_grid_index_list.push_back(index);
+      index++;
+    }
+  }
+
+  return rect_grid_index_list;
+}
+
+int ObstacleInflater::get_upper_right_grid_index(
+    const int center_grid_index, const float target_margin, const nav_msgs::OccupancyGrid &map)
+{
+  if (center_grid_index < 0 || map.data.size() <= center_grid_index)
+  {
+    ROS_WARN("center_grid_index is out of range");
+    return -1;
+  }
+
+  const int target_margin_cell = static_cast<int>(target_margin / map.info.resolution);
+
+  int index = center_grid_index + target_margin_cell * map.info.width;
+  if (map.data.size() <= index)
+    index = map.info.width * (map.info.height - 1) + calc_grid_index_x(center_grid_index, map);
+
+  const int max_index = calc_max_grid_index_in_same_line(index, map);
+  index = std::min(index + target_margin_cell, max_index);
+
+  return index;
+}
+
+int ObstacleInflater::get_lower_left_grid_index(
+    const int center_grid_index, const float target_margin, const nav_msgs::OccupancyGrid &map)
+{
+  if (center_grid_index < 0 || map.data.size() <= center_grid_index)
+  {
+    ROS_WARN("center_grid_index is out of range");
+    return -1;
+  }
+
+  const int target_margin_cell = static_cast<int>(target_margin / map.info.resolution);
+
+  int index = center_grid_index - target_margin_cell * map.info.width;
+  if (index < 0)
+    index = calc_grid_index_x(center_grid_index, map);
+
+  const int min_index = calc_min_grid_index_in_same_line(index, map);
+  index = std::max(index - target_margin_cell, min_index);
+
+  return index;
+}
+
+int ObstacleInflater::calc_grid_index_x(const int index, const nav_msgs::OccupancyGrid &map)
+{
+  return index % map.info.width;
+}
+
+int ObstacleInflater::calc_grid_index_y(const int index, const nav_msgs::OccupancyGrid &map)
+{
+  return static_cast<int>(index / map.info.width);
+}
+
+int ObstacleInflater::calc_max_grid_index_in_same_line(const int index, const nav_msgs::OccupancyGrid &map)
+{
+  const int index_y = calc_grid_index_y(index, map);
+  return (index_y + 1) * map.info.width - 1;
+}
+
+int ObstacleInflater::calc_min_grid_index_in_same_line(const int index, const nav_msgs::OccupancyGrid &map)
+{
+  const int index_y = calc_grid_index_y(index, map);
+  return index_y * map.info.width;
+}
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "obstacle_inflater");
+  ObstacleInflater obstacle_inflater;
+  obstacle_inflater.process();
+
+  return 0;
+}
